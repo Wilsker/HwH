50a446618b14de9655297be2ecda31c261487609
Joshuha <jthomasw@cern.ch>
2022-04-01 15:51:14 +0200

    Update with corrected gridpacks w/wo restriction cards, include restriction card in model dir, correct implementation of card in MG process cards

---
+++ UFO_models/H^6_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 11.3.0 for Linux x86 (64-bit) (March 7, 2018)
+# Date: Wed 12 Dec 2018 23:43:53
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+QNP = CouplingOrder(name = 'QNP',
+                    expansion_order = 2,
+                    hierarchy = 1)
+
